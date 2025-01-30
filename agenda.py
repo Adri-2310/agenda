334d61a5e08fc6c2f0e6420f9abaae12e1531e5a
AdrienMertens <mertensadrien23@hotmail.be>
2025-01-30 11:47:26 +0100

    les mois exclu février et decembre fonctionne

--- agenda.py
+++ agenda.py
@@ -8,23 +8,39 @@ month = int(input("Enter a month: "))
 year = int(input("Enter a year: "))
 
 #
-flag = False
+flag: bool = False
 
 # add a day relative to the date given by the user
 if month != 2: # all other months without february
     if month != 12 : # the december month
         if month in (1, 3, 5, 7, 8, 10): # the months of 31 days
-            if day >= 30 : # traite le cas du 31 jour
+            if day < 31 and day > 0: # traite le cas en-desous de 31
+                day += 1
+                flag = True
+            elif day == 31 : # traite le cas du 31 jour
                 day = 1
                 month += 1
                 flag = True
-            else: # traite le cas des jours en desous de 30
-                pass
-        else: # the months of 30 days
-            pass
+            else : # l'utilisateur a mis un mauvais jour
+                flag = False # to do mauvais jour entrez
+        elif month in (4,6,9,11): # the months of 30 days
+            if day < 30 and day > 0:  # traite le cas en-desous de 30
+                day += 1
+                flag = True
+            elif day == 30:  # traite le cas du 30 jour
+                day = 1
+                month += 1
+                flag = True
+            else:  # l'utilisateur a mis un mauvais jour
+                flag = False  # to do mauvais jour entrez
+        else:
+                flag = False # l'utilisateur a mis un nombre trop grand pour le month
     else: # all other months without December
         pass
 else: # the february month
     pass
 
-if flag : print(f"demain, il sera {day}/{month}/{year}")
+if flag :
+    print(f"demain, il sera {day}/{month}/{year}")
+else :
+    print(f"Vous avez entré de mauvais données, les données doivent être entré comme cela jour:30 / month:3 / year:2000")
